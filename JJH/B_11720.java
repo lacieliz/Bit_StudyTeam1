53b3af518aa5e89682b70be563bc462be6b5620e
junhwan2000 <j8428820@naver.com>
2025-02-09 19:04:57 +0900

    [JJH] B_11720 upload

--- JJH/B_11720.java
+++ JJH/B_11720.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Main {
+public class B_11720 {
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
