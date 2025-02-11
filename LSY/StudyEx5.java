5be34c50c8c60b7482b56e9f45c55240d52a9249
seolyeong333 <33qqpp@naver.com>
2025-02-11 18:44:13 +0900

    [LSY] StudyEx5 upload

--- LSY/StudyEx5.java
+++ LSY/StudyEx5.java
@@ -1,17 +1,7 @@
 public class main {
 
 	public static void main(String[] args) {
-		// 5번째																			// if else 를 사용해서 if else 각각에 for 문을 사용하면 쉽기는 함. 그치만 하나로도 가능.
-		// 중간 두줄을 주고, 가운데를 주면 되겠는데?
-		int SIZE = 5;
-		int m[][] = new int [SIZE][SIZE];
-		int cnt = 0;
-		for( int i=0; i<m.length; i++ ) {
-		for( int j=0; j<m[i].length; j++ ) {
-			if( j == SIZE / 2 || i == SIZE / 2 || ! ( i == 0 || i == SIZE-1 || j == 0 || j == SIZE-1  ) )
-			m[i][j] = ++cnt;
-			}
-		}
+
 		
 		// 출력
 		for( int i=0; i<m.length; i++ ) {
