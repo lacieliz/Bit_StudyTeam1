4ba6d0100c0bb32930ed93fefdbeb693e4d7c7e6
junhwan2000 <j8428820@naver.com>
2025-02-11 18:52:55 +0900

    [JJH] StudyEx5 add even num calc

--- JJH/StudyEx5.java
+++ JJH/StudyEx5.java
@@ -11,8 +11,12 @@ public class ArrayEx5 {
 		// 입력
 		for (int i = 0; i < m.length; i++) {
 			for (int j = 0; j < m.length; j++) {
-				if( j-i <= SIZE/2 && i-j <= SIZE/2 && i+j >= SIZE/2 && i+j <= SIZE/2*3 ) m[i][j] = ++cnt;				
-				//   좌하단 제외      우상단 제외       좌상단 제외        우하단 제외        
+				if(SIZE%2==1) { // SIZE -> 홀수
+					if( j-i <= SIZE/2 && i-j <= SIZE/2 && i+j >= SIZE/2 && i+j <= SIZE/2*3 ) m[i][j] = ++cnt;	
+				}		//   좌하단 제외       우상단 제외         좌상단 제외        우하단 제외  
+				else {	// SIZE -> 짝수
+					if( j-i <= SIZE/2 && i-j <= SIZE/2 && i+j >= SIZE/2-1 && i+j <= SIZE+(SIZE/2-1) ) m[i][j] = ++cnt;
+				}      
 			}
 		}
 	
