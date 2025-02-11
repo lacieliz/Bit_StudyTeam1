e131263660d1e996ab62fdf59852392810176157
junhwan2000 <j8428820@naver.com>
2025-02-11 18:11:17 +0900

    [JJH] StudyEx5 upload

--- JJH/StudyEx5.java
+++ JJH/StudyEx5.java
@@ -5,13 +5,14 @@ import java.util.Iterator;
 public class ArrayEx5 {
 
 	public static void main(String[] args) {
-		int SIZE = 5;
+		int SIZE = 7;
 		int m[][] = new int[SIZE][SIZE];
 		int cnt = 0;
 		// 입력
 		for (int i = 0; i < m.length; i++) {
 			for (int j = 0; j < m.length; j++) {
-				if( j-i <= SIZE/2 && i-j <= SIZE/2 && i+j >= SIZE/2 && i+j <= SIZE/2*3 ) m[i][j] = ++cnt;	
+				if( j-i <= SIZE/2 && i-j <= SIZE/2 && i+j >= SIZE/2 && i+j <= SIZE/2*3 ) m[i][j] = ++cnt;				
+				//   좌하단 제외      우상단 제외       좌상단 제외        우하단 제외        
 			}
 		}
 	
