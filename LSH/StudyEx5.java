6eeffd7e0aec126573c60454a29f2b17eb741b39
lacieliz <lacieliz@naver.com>
2025-02-11 22:23:11 +0900

    add SIZE/2 -> int mid

--- LSH/StudyEx5.java
+++ LSH/StudyEx5.java
@@ -6,17 +6,17 @@ public class ArrayEx5 {
 		int SIZE = 7;
 		int m[][] = new int[SIZE][SIZE];
 		int cnt = 0;
+		int mid = SIZE/2;
 		//입력
 		//5번
 		for(int i=0; i<m.length; i++) {
 			for(int j=0; j<m[i].length ;j++) {
-
-	 		if(i==SIZE/2)
-					m[i][j] = ++cnt;
-				else if(((i+j)>=(SIZE/2)) && ((j-i)<=(SIZE/2)) && i<(SIZE/2))
-					m[i][j] = ++cnt;
-				else if(((i-j)<=(SIZE/2)) && (i+j)<=(SIZE/2*3) && i>(SIZE/2))
-					m[i][j] = ++cnt;
+	 		if(i==mid)
+				m[i][j] = ++cnt;
+			else if(((i+j)>=mid) && ((j-i)<=mid) && i<mid)
+				m[i][j] = ++cnt;
+			else if(((i-j)<=mid) && (i+j)<=(mid*3) && i>(mid))
+				m[i][j] = ++cnt;
 			}
 		}
 		//출력
