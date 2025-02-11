1cc319bd8907c4918e5cb380993b26a9f93e0ea8
lacieliz <lacieliz@naver.com>
2025-02-11 21:35:51 +0900

    [LSH]StudyEx5 for odd number

--- LSH/StudyEx5.java
+++ LSH/StudyEx5.java
@@ -3,16 +3,19 @@ package java08;
 public class ArrayEx5 {
 
 	public static void main(String[] args) {
-		int SIZE = 5;
+		int SIZE = 7;
 		int m[][] = new int[SIZE][SIZE];
 		int cnt = 0;
 		//입력
 		//5번
 		for(int i=0; i<m.length; i++) {
 			for(int j=0; j<m[i].length ;j++) {
-				if(i==SIZE/2 || j==SIZE/2)
+
+	 		if(i==SIZE/2 || j==SIZE/2)
 					m[i][j] = ++cnt;
-				else if(i*j%2==1)
+				else if(((i+j)>=(SIZE/2)) && ((j-i)<=(SIZE/2)) && i<(SIZE/2))
+					m[i][j] = ++cnt;
+				else if(((i-j)<=(SIZE/2)) && (i+j)<=(SIZE/2*3) && i>(SIZE/2))
 					m[i][j] = ++cnt;
 			}
 		}
