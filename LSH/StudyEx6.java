1bbfeade3fd07189170c83231fb30c0ecb79534b
lacieliz <lacieliz@naver.com>
2025-02-12 17:17:29 +0900

    [LSH]StudyEx6 reupload

--- LSH/StudyEx6.java
+++ LSH/StudyEx6.java
@@ -1,5 +1,43 @@
 package java09;
-
+class Car{
+	protected String name;
+	protected String fuel;
+	protected int ton;
+	protected int line;
+	
+	public String getName() {
+		return name;
+	}
+	public String getFuel() {
+		return fuel;
+	}
+	public int getCarry() {
+		return ton;
+	}
+	public int getNumber() {
+		return line;
+	}
+}
+class Truck extends Car{
+	public Truck(String name, String fuel, int ton) {
+		this.name=name;
+		this.fuel=fuel;
+		this.ton=ton;
+	}
+}
+class Bus extends Car{
+	public Bus(String name, String fuel, int line) {
+		this.name=name;
+		this.fuel=fuel;
+		this.line=line;
+	}
+}
+class Bike extends Car{
+	public Bike(String name, String fuel) {
+		this.name=name;
+		this.fuel=fuel;
+	}
+}
 public class InherStudy {
 	public static void main(String[] args) {		
 		// Car 클래스를 상속 받은 Truck, Bus, Bike 클래스를 구현한다. 
